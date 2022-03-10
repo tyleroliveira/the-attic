e53d241465900f763b31142e594ffb324cc7f9b0
Ava <avamsteimle@gmail.com>
2022-03-10 11:52:31 -0800

    setting up server connection and file formatting

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,4 @@
-// Exports every other js module in the models folder.
-const fs = require("fs");
+const Box = require('./Box');
+const User = require('./User');
 
-const modelEntries = fs
-  .readdirSync(__dirname)
-  .filter((filename) => /.js$/.test(filename))
-  .map((filename) => [filename.replace(/.js$/, ""), require(`./${filename}`)]);
-
-module.exports = Object.fromEntries(modelEntries);
+module.exports = { Box, User };
