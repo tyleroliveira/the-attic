7cd649c750c1c3d8f9b05e48c453b31a49e54e92
Ava <avamsteimle@gmail.com>
2022-03-10 19:19:06 -0800

    box model done

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/the-attic',
-  {
+  .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/the-attic', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
--- server/models/Box.js
+++ server/models/Box.js
@@ -4,7 +4,28 @@ const {
 } = require("mongoose");
 
 const boxSchema = new Schema({
-
+    title: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    },
+    items: [{
+        title: {
+            type: String,
+            required: true,
+            minLength: 1
+        },
+        itemCode: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        itemLink: {
+            type: URL,
+        }
+    }, ],
 });
 
 const Box = model("Box", boxSchema);
