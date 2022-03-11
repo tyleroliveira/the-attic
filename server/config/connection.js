464c3a00fb189889a1b6f2773d6217da0390c7ae
TylerOliveira <55139667+TylerOliveira@users.noreply.github.com>
2022-03-11 10:27:04 -0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,8 +4,6 @@ mongoose
   .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/the-attic', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
   })
   .catch((err) => {
     console.log(err);
