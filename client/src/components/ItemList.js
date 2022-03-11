383021d324bcbc158d7c4031ee51f48467848aa4
Ava <avamsteimle@gmail.com>
2022-03-11 14:14:10 -0800

    saved

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -7,24 +7,12 @@ const ItemList = ({ items = [] }) => {
 
   return (
     <>
-      <h3
-        className="p-5 display-inline-block"
-        style={{ borderBottom: '1px dotted #1a1a1a' }}
-      >
-        JavaScript
-      </h3>
       <div className="flex-row my-4">
         {items &&
-          items.map((comment) => (
-            <div key={comment._id} className="col-12 mb-3 pb-3">
+          items.map((item) => (
+            <div key={item._id} className="col-12 mb-3 pb-3">
               <div className="p-3 bg-dark text-light">
-                <h5 className="card-header">
-                  {comment.commentAuthor} commented{' '}
-                  <span style={{ fontSize: '0.825rem' }}>
-                    on {comment.createdAt}
-                  </span>
-                </h5>
-                <p className="card-body">{comment.commentText}</p>
+                <p className="card-body">{item}</p>
               </div>
             </div>
           ))}
