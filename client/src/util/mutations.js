7e1e832d7df627e281f549478b18d7fa3541bcff
Lily Thomas <lilylynettethomas@gmail.com>
2022-03-12 14:08:36 -0800

    Added queries and mutations

--- client/src/util/mutations.js
+++ client/src/util/mutations.js
@@ -18,9 +18,56 @@ export const LOGIN = gql `
   }
 `;
 
-// addBox
-// addItem
-// updateBox
-// updateItem
-// removeBox
-// removeItem 
+export const ADD_BOX = gql`
+  mutation AddBox($title: String!) {
+    addBox(title: $title) {
+      _id
+      title
+    }
+  }
+`;
+
+export const ADD_ITEM = gql`
+  mutation AddBox($boxId: ID!, $title: String!) {
+    addItem(BoxId: $boxId, title: $title) {
+      _id
+      title
+    }
+  }
+`;
+
+export const UPDATE_BOX = gql`
+  mutation AddBox($id: ID!, $title: String!) {
+    updateBox(_id: $id, title: $title) {
+      _id
+      title
+    }
+  }
+`;
+
+export const UPDATE_ITEM = gql`
+  mutation AddBox($id: ID!, $title: String!) {
+    updateItem(_id: $id, title: $title) {
+      _id
+      title
+    }
+  }
+`;
+
+export const REMOVE_BOX = gql`
+  mutation AddBox($boxId: ID!) {
+    removeBox(boxId: $boxId) {
+      _id
+      title
+    }
+  }
+`;
+
+export const REMOVE_ITEM = gql`
+  mutation AddBox($boxId: ID!, $itemId: ID!) {
+    removeItem(boxId: $boxId, itemId: $itemId) {
+      _id
+      title
+    }
+  }
+`;
