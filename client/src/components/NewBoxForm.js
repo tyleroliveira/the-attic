76dd4f712af9e02e22846d6e6ad0112874953d3f
Ava <avamsteimle@gmail.com>
2022-03-13 15:44:40 -0700

    box form (400 err)

--- client/src/components/NewBoxForm.js
+++ client/src/components/NewBoxForm.js
@@ -1,38 +1,99 @@
-// import React, { useState, useEffect } from 'react';
-import { Form, Button} from 'react-bootstrap';
-// import { useMutation } from '@apollo/client';
-// import { ADD_BOX } from '../utils/mutations';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+
+import { ADD_BOX } from '../util/mutations';
+import { GET_BOXES, ME } from "../util/queries"
+
+import useAuth  from '../util/auth';
 
 const NewBoxForm = () => {
     // Use state to set initial state to empty title string
+    const [boxTitle, setBoxTitle] = useState("");
 
     // handleInputChange for user to input title
+    const [addBox, { error }] = useMutation(ADD_BOX, {
+        update(cache, { data: { addBox } }) {
+            try {
+                const { boxes } = cache.readQuery({ query: GET_BOXES });
 
-    //handle form submit to add box to user's box page
+                cache.writeQuery({
+                    query: GET_BOXES,
+                    data: { boxes: [addBox, ...boxes] },
+                });
+            } catch (err) {
+                console.log(err);
+            }
 
-    //reset form data to initial state
+                  // update me object's cache
+      const me = cache.readQuery({ query: ME })?.me;
+      if (!me) {
+        // no me query in the cache yet. abort update.
+        return;
+      }
+      cache.writeQuery({
+        query: ME,
+        data: { me: { ...me, boxes: [...me.boxes, addBox] } },
+      });
+    },
+});
+
+//handle form submit to add box to user's box page
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addBox({
+        variables: {
+          boxTitle,
+        },
+      });
+
+      //reset form data to initial state
+      setBoxTitle('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // handle input change
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'boxTitle' && value.length <= 280) {
+      setBoxTitle(value);
+    }
+  };
+   
 
     return (
 
-          <Form>
-          <Form.Group>
-          <Form.Label htmlFor='title'>Title:</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='your title'
-            name='title'
-            // onChange={handleInputChange}
-            // value={userFormData.email}
-            required
-          />
-        </Form.Group>
-          <Button
-            type="button" 
-            className="btn btn-dark" 
-            data-dismiss="modal">
-              +
-          </Button>
-          </Form>
+        <form
+        className="flex-row justify-center justify-space-between-md align-center"
+        onSubmit={handleFormSubmit}
+      >
+        <div className="col-12 col-lg-9">
+          <textarea
+            name="boxTitle"
+            placeholder="your title here.."
+            value={boxTitle}
+            className="form-input w-100"
+            style={{ lineHeight: '1.5', resize: 'vertical' }}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+
+        <div className="col-12 col-lg-3">
+          <button className="btn btn-dark" type="submit">
+            +
+          </button>
+        </div>
+        {error && (
+          <div className="col-12 my-3 bg-danger text-white p-3">
+            {error.message}
+          </div>
+        )}
+      </form>
     )
 
 }
