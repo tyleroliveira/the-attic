43ed9b236680c5889d7d787a9bd73d57ca30c9a4
Ava <avamsteimle@gmail.com>
2022-03-14 20:06:55 -0700

    unfinished new item form, needs to handle input change

--- client/src/components/NewItemForm.js
+++ client/src/components/NewItemForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
+import { Link } from 'react-router-dom';
 
 import { ADD_ITEM } from '../util/mutations';
 
@@ -7,6 +8,8 @@ import {useAuth} from '../util/auth';
 
 const NewItemForm = ({ boxId }) => {
   const [itemText, setItemText] = useState('');
+  const [itemCode, setItemCode] = useState('');
+  const [itemLink, setItemLink] = useState('');
 
   const [addItem, { error }] = useMutation(ADD_ITEM);
 
@@ -18,46 +21,73 @@ const NewItemForm = ({ boxId }) => {
         variables: {
           boxId,
           itemText,
+          itemCode,
+          itemLink
         },
       });
 
       setItemText('');
+      setItemCode('');
+      setItemLink('');
     } catch (err) {
       console.error(err);
     }
   };
 
   const handleChange = (event) => {
-    const { name, value } = event.target;
+    const { title, code, link, value } = event.target;
 
-    if (name === 'itemText' && value.length <= 280) {
+    if (title === 'itemText' && code === 'itemCode' && link === "itemLink" && value.length <= 280) {
       setItemText(value);
+      setItemCode(value);
+      setItemLink(value);
     }
   };
 
   return (
     <div>
       <h4>Add an item to your box?</h4>
-
         <>
           <form
             className="flex-row justify-center justify-space-between-md align-center"
             onSubmit={handleFormSubmit}
           >
             <div className="col-12 col-lg-9">
-              <textarea
+              <input
                 name="itemText"
-                placeholder="Add your item..."
+                placeholder="Add your title..."
                 value={itemText}
                 className="form-input w-100"
                 style={{ lineHeight: '1.5', resize: 'vertical' }}
                 onChange={handleChange}
-              ></textarea>
+              ></input>
+            </div>
+            <div className="col-12 col-lg-9">
+              <input
+                name="itemCode"
+                placeholder="Add your code.."
+                value={itemCode}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div className="col-12 col-lg-9">
+              <input
+                name="itemLink"
+                placeholder="Add your link.."
+                value={itemLink}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></input>
             </div>
 
             <div className="col-12 col-lg-3">
-              <button className="btn btn-primary btn-block py-3" type="submit">
-                Add Item
+              <button 
+              className="btn btn-dark" 
+              type="submit">
+                +
               </button>
             </div>
           </form>
